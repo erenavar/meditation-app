c2c717a42d6ebbc483a234e88f0237edb3af3c9a
Eren AVAR <eren.avar@hotmail.com>
2024-09-23 17:11:00 +0300

    add login and forgotpassword screen stacks and create types of navigation file

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,14 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
+import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
+import { RootStackParamList } from "./types";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function MainNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/navigation/types.ts
@@ -0,0 +1,7 @@
+import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
+import LoginScreen from "../screens/LoginScreen";
+
+export type RootStackParamList = {
+    LoginScreen: undefined;
+    ForgotPasswordScreen: undefined
+}
