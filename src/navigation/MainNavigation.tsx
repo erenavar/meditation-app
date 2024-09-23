d5aae8c0fcde53825753f82d5b44d7da759ef74c
Eren AVAR <eren.avar@hotmail.com>
2024-09-23 16:51:45 +0300

    install stack navigation

---
+++ src/navigation/MainNavigation.tsx
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../screens/LoginScreen";
+
+const Stack = createStackNavigator();
+
+function MainNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainNavigation;
