d773d957555e642570c56a9a7173ed36b481b1d3
Eren AVAR <eren.avar@hotmail.com>
2024-09-23 22:31:45 +0300

    add login image and adjust flex ratios to login screen

--- src/screens/loginScreen/LoginScreen.tsx
+++ src/screens/loginScreen/LoginScreen.tsx
@@ -5,12 +5,35 @@ import { useNavigation } from "@react-navigation/native";
 const LoginScreen = () => {
   const navigation = useNavigation();
   return (
-    <SafeAreaView>
-      <Text>LoginScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoArea}>
+        <Text style={styles.text}>Welcome</Text>
+      </View>
+      <View style={styles.actionArea}></View>
     </SafeAreaView>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logoArea: {
+    flex: 3,
+    backgroundColor: "#0092C9",
+  },
+  text: {
+    color: "white",
+    fontFamily: "Poppins",
+    fontSize: 40,
+    fontWeight: "900",
+    left: 40,
+    top: 160,
+  },
+  actionArea: {
+    flex: 2,
+    backgroundColor: "pink",
+  },
+});
