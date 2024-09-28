fb6d6dc94bcbe6d5adc99201abd9352fcdf11302
Eren AVAR <eren.avar@hotmail.com>
2024-09-28 21:34:17 +0300

    add tab bar icons

--- src/navigation/TabBarNavigation.tsx
+++ src/navigation/TabBarNavigation.tsx
@@ -2,14 +2,31 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/homeScreen/HomeScreen";
 import ProfileScreen from "../screens/profileScreen/ProfileScreen";
 import { TabParamList } from "./types";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
 function TabBarNavigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" size={24} color="black" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
