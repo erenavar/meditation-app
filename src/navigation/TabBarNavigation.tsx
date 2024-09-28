7a0cc6839f704ece5fed42992e1f31a9ede1e094
Eren AVAR <eren.avar@hotmail.com>
2024-09-28 22:23:44 +0300

    adjust tab bar icons and labels align

--- src/navigation/TabBarNavigation.tsx
+++ src/navigation/TabBarNavigation.tsx
@@ -3,18 +3,28 @@ import HomeScreen from "../screens/homeScreen/HomeScreen";
 import ProfileScreen from "../screens/profileScreen/ProfileScreen";
 import { TabParamList } from "./types";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Platform } from "react-native";
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
 function TabBarNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          marginBottom: Platform.OS === "android" ? 15 : undefined,
+        },
+        tabBarStyle: {
+          height: Platform.OS === "android" ? 65 : 80,
+        },
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="home" size={24} color="black" />
+            <MaterialIcons name="home" size={30} color="black" />
           ),
         }}
       />
@@ -23,7 +33,7 @@ function TabBarNavigation() {
         component={ProfileScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="person" size={24} color="black" />
+            <MaterialIcons name="person" size={30} color="black" />
           ),
         }}
       />
