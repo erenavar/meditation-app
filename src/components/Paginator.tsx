41eefa01c0ea23b6003988b0caa0f53cb1c8604a
Eren AVAR <eren.avar@hotmail.com>
2024-10-14 18:02:51 +0300

    create map function and css on paginator and pass props

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Paginator = () => {
+const Paginator = ({ data }) => {
   return (
     <View>
-      <Text>Paginator</Text>
+      {data.map((_:any, i:number) => {
+        return <View style={styles.dot} key={i.toString()} />;
+      })}
     </View>
   );
 };
 
 export default Paginator;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 64,
+  },
+  dot: {
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "493d8a",
+    marginHorizontal: 8,
+  },
+});
--- src/screens/carousel/IntroScreen.tsx
+++ src/screens/carousel/IntroScreen.tsx
@@ -2,6 +2,7 @@ import { Animated, FlatList, StyleSheet, Text, View } from "react-native";
 import React, { useRef, useState } from "react";
 import datas from "../../utils/introTexts";
 import Carousel from "../../components/Carousel";
+import Paginator from "../../components/Paginator";
 
 const IntroScreen = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -36,6 +37,7 @@ const IntroScreen = () => {
           ref={slidesRef}
         />
       </View>
+      <Paginator data={slides} />
     </View>
   );
 };
