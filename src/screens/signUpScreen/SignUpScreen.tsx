f7c6faacf69a6392143ab95d1b984b03b2644650
Eren AVAR <eren.avar@hotmail.com>
2024-11-12 19:28:19 +0300

    add arrow and adjust its position

--- src/screens/signUpScreen/SignUpScreen.tsx
+++ src/screens/signUpScreen/SignUpScreen.tsx
@@ -1,15 +1,31 @@
-import { StyleSheet, View } from "react-native";
 import React from "react";
+import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
 import Input from "../../components/Input";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+const { width, height } = Dimensions.get("window");
 
 const SignUpScreen = () => {
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
+      <AntDesign
+        name="arrowleft"
+        size={34}
+        color="black"
+        style={styles.arrow}
+      />
       <Input />
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default SignUpScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: width * 0.04,
+  },
+  arrow: {
+    marginTop: height * 0.04,
+  },
+});
