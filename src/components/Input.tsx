54b04518bb0e64fde92d639d3a6f3144447af7fd
Eren AVAR <eren.avar@hotmail.com>
2024-11-12 18:43:47 +0300

    create foundation of button component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, TextInput, View } from "react-native";
 import React from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { useNavigation } from "@react-navigation/native";
 
 const Input = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <TextInput />
+      <FontAwesome6 name="user" size={20} color="black" />
+      <TextInput placeholder="name" style={styles.input} />
     </View>
   );
 };
@@ -13,7 +17,17 @@ export default Input;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "red",
-    fontFamily: "bold",
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "pink",
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+    paddingBottom: 10,
+    alignItems: "center",
+    left: 30,
+  },
+  input: {
+    fontSize: 17,
+    marginLeft: 10,
   },
 });
