f5f252c7b5437d0cbdf68630e2c7ad238a7fbeb4
Eren AVAR <eren.avar@hotmail.com>
2025-06-16 00:52:24 +0300

    create line component to seperate lines

---
+++ src/components/Line.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function Line() {
+  return (
+    <View
+      style={{
+        borderBottomColor: "#B0C9C7",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        alignSelf: "center",
+        width: "90%",
+      }}
+    />
+  );
+}
