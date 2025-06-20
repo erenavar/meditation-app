5c6bf5956a2638ee40bdb70192f672eba9e29a12
Eren AVAR <eren.avar@hotmail.com>
2025-06-20 15:04:07 +0300

    create preview card for blog screen and articles.json

---
+++ src/components/PreviewCard.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const PreviewCard = () => {
+  return (
+    <View>
+      <Text>PreviewCard</Text>
+    </View>
+  );
+};
+
+export default PreviewCard;
+
+const styles = StyleSheet.create({});
--- src/screens/blogScreen/BlogScreen.tsx
+++ src/screens/blogScreen/BlogScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import PreviewCard from "../../components/PreviewCard";
 
 function BlogScreen() {
   return (
     <View>
-      <Text>Blog Screen</Text>
+      <PreviewCard />
     </View>
   );
 }
