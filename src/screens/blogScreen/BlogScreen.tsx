d0a5f6b261a08bc13ba75f5cece4897fe7ae8c49
Eren AVAR <eren.avar@hotmail.com>
2025-06-26 22:33:58 +0300

    try different css and placing on blog screen

--- src/screens/blogScreen/BlogScreen.tsx
+++ src/screens/blogScreen/BlogScreen.tsx
@@ -1,11 +1,11 @@
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
 import React from "react";
 import PreviewCard from "../../components/PreviewCard";
 
 const BlogScreen = () => {
   return (
     <SafeAreaView>
-      <ScrollView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <PreviewCard />
         <PreviewCard />
         <PreviewCard />
@@ -20,7 +20,9 @@ export default BlogScreen;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#E5F4F2",
-    padding: 30,
+    height: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
   },
 });
