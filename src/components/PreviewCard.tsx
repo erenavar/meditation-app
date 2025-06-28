2a2abbf95a63247e41162dd1b37ddc422c84a307
Eren AVAR <eren.avar@hotmail.com>
2025-06-28 15:45:51 +0300

    complete preview card

--- src/components/PreviewCard.tsx
+++ src/components/PreviewCard.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import articles from "../../assets/blog/articles.json";
 import { IInstance } from "./types";
 
@@ -15,18 +15,57 @@ const PreviewCard = () => {
 
   return (
     <View style={styles.container}>
-      <Image></Image>
-      <View></View>
+      <View style={styles.imgArea}>
+        <Image source={image} style={styles.img} resizeMode="stretch" />
+      </View>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Article", { name: instance.city });
+        }}>
+        <View style={styles.titleArea}>
+          <Text style={styles.title}>{instance.title}</Text>
+          <Text style={styles.date}>{instance.publishedAt}</Text>
+        </View>
+
+        <Text numberOfLines={3} style={{ marginTop: 10 }}>
+          {instance.description}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
 export default PreviewCard;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    height: "30%",
+    height: "60%",
     borderRadius: 20,
+    padding: 10,
+  },
+  imgArea: {
+    width: "100%",
+    height: "70%",
+  },
+  img: {
+    width: "100%",
+    height: "90%",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  titleArea: {
+    flexDirection: "row",
+    marginVertical: 5,
+  },
+  title: {
+    fontWeight: "bold",
+    letterSpacing: 0.1,
+    fontSize: 16,
+    flex: 3,
+  },
+  date: {
+    flex: 1,
+    left: "auto",
+    color: "gray",
   },
 });
